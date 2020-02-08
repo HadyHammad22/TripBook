2be27b699841ccab7d57aa49cf513f5a5f0ceabb
HadyHammad <hady.fci@gmail.com>
2020-02-08 15:08:13 -0800

    Implement CoreLocation and Choosing location

--- TripBook/ViewController.swift
+++ TripBook/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
@@ -19,11 +20,17 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //mapView and location manager attributes
         mapView.delegate = self
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(getsureRecognizer:)))
+        recognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(recognizer)
     }
     // Location Manager Delegate
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -31,6 +38,19 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         let region = MKCoordinateRegion(center: location, span: span)
         self.mapView.setRegion(region, animated: true)
+    }
+    
+    @objc func chooseLocation(getsureRecognizer: UILongPressGestureRecognizer){
+        if getsureRecognizer.state == UIGestureRecognizer.State.began{
+            let touchedPoint = getsureRecognizer.location(in: self.mapView)
+            let choosenCoordinates = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = choosenCoordinates
+            annotation.title = "New Annotation Here"
+            annotation.subtitle = "This is where i've choosen"
+            self.mapView.addAnnotation(annotation)
+        }
+        
     }
 
 }
