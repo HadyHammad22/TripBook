d0f7c2545f28ed567c7c749e4747ae1b61e7d4dd
HadyHammad <hady.fci@gmail.com>
2020-02-08 17:18:35 -0800

    add first view controller file

---
+++ TripBook/firstVC.swift
@@ -0,0 +1,30 @@
+//
+//  firstVC.swift
+//  TripBook
+//
+//  Created by HadyHammad on 2/8/20.
+//  Copyright © 2020 HadyHammad. All rights reserved.
+//
+
+import UIKit
+
+class firstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
